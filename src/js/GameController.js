2bcab761d074213fc768fd9f846608a2e5d17554
Aleksandr Korenev <korenev6052@gmail.com>
2020-01-21 01:28:58 +0300

    7. Отрисовка команд

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,4 +1,5 @@
-import themes from "./themes";
+import themes from './themes';
+import { UserTeam, ComputerTeam } from './Team';
 
 export default class GameController {
   constructor(gamePlay, stateService) {
@@ -10,6 +11,20 @@ export default class GameController {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateServic
     this.gamePlay.drawUi(themes.prairie);
+    const user = new UserTeam();
+    console.log(JSON.stringify(user.characters));
+    const computer = new ComputerTeam();
+    console.log(JSON.stringify(computer.characters));
+
+    user.updateByRound(2);
+    console.log(JSON.stringify(user.characters));
+    computer.updateByRound(2, user.livingCharactersCount);
+    console.log(JSON.stringify(computer.characters));
+
+    user.updateByRound(3);
+    console.log(JSON.stringify(user.characters));
+    computer.updateByRound(3, user.livingCharactersCount);
+    console.log(JSON.stringify(computer.characters));
   }
 
   onCellClick(index) {
