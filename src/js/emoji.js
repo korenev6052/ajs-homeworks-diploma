0dd808c5fdbc38624de9f5d91e1ea53713d66837
Aleksandr Korenev <korenev6052@gmail.com>
2020-01-23 22:43:00 +0300

    8. Вывод информации о персонаже

---
+++ src/js/emoji.js
@@ -0,0 +1,8 @@
+const emoji = {
+  medal: String.fromCharCode(0xD83C, 0xDF96),
+  swords: String.fromCharCode(0x2694),
+  shield: String.fromCharCode(0xD83D, 0xDEE1),
+  heart: String.fromCharCode(0x2764),
+};
+
+export default emoji;
