691f9f664573dcbefdfb17c4b59a38e485941a73
Aleksandr Korenev <korenev6052@gmail.com>
2020-01-24 23:07:25 +0300

    9. Выбор персонажа

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,10 +1,9 @@
-import { Bowman, Daemon, Magician, Swordsman, Vampire, Undead } from './Character';
-import { characterGenerator, generateTeam } from './generators';
+import Character, { Bowman, Daemon, Magician, Swordsman, Vampire, Undead } from './Character';
 
 export default class Team {
   constructor(player, allowedTypes) {
-    this._player = player;
-    this._allowedTypes = allowedTypes;
+    this.player = player;
+    this.allowedTypes = allowedTypes;
     this._characters = new Set();
 
     if (new.target.name === 'Team') {
@@ -20,58 +19,46 @@ export default class Team {
     return this._characters.size;
   }
 
+  get livingCharacters() {
+    return this.characters.filter((character) => character.isLiving());
+  }
+
   get livingCharactersCount() {
-    let count = 0;
-    this._characters.forEach((character) => {
+    return this.characters.reduce((count, character) => {
       if (character.isLiving()) count += 1;
-    });
-    return count;
+    }, 0);
+  }
+
+  addCharacter(character) {
+    if (!(character instanceof Character)) {
+      throw new Error('Character must be instance of Character or its children');
+    }
+
+    this._characters.add(character);
+  }
+
+  addCharacters(characters) {
+    characters.forEach((character) => this.addCharacter(character));
   }
 
   charactersLevelUp() {
     this._characters.forEach((character) => {
       if (character.isLiving()) {
-        character.restoreCurrentHealth();
+        character.restoreHealth();
         character.levelUp();
       }
     });
   }
 }
 
-export class UserTeam extends Team {
+export class User extends Team {
   constructor() {
     super('user', [Swordsman, Bowman, Magician]);
-    this.updateByRound(1);
-  }
-
-  updateByRound(round) {
-    switch (round) {
-      case 1:
-        this._characters.add(new Bowman(1));
-        this._characters.add(new Swordsman(1));
-        break;
-      case 2:
-        this.charactersLevelUp();
-        const generator = characterGenerator(this._allowedTypes, 1);
-        this._characters.add(generator.next().value);
-        break;
-      default:
-        this.charactersLevelUp();
-        generateTeam(this._allowedTypes, round - 1, 2)
-          .forEach((character) => this._characters.add(character));
-    }
   }
 }
 
-export class ComputerTeam extends Team {
+export class Engine extends Team {
   constructor() {
-    super('computer', [Daemon, Undead, Vampire]);
-    this.updateByRound(1, 2);
-  }
-
-  updateByRound(round, userCharactersCount) {
-    this._characters.clear();
-    generateTeam(this._allowedTypes, round, userCharactersCount)
-      .forEach((character) => this._characters.add(character));
+    super('engine', [Daemon, Undead, Vampire]);
   }
 }
