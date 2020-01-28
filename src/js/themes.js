108ed5c7db7f2689605685746e07b99673235506
Aleksandr Korenev <korenev6052@gmail.com>
2020-01-29 00:41:34 +0300

    14. Game Loop - 15. Game Over, New Game и статистика

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,8 +1,8 @@
 const themes = {
-  prairie: 'prairie',
-  desert: 'desert',
-  arctic: 'arctic',
-  mountain: 'mountain',
+  round1: 'prairie',
+  round2: 'desert',
+  round3: 'arctic',
+  round4: 'mountain',
 };
 
 export default themes;
