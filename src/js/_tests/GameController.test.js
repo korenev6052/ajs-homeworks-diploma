f220aa7a884fc53a7968bd614e0b9575e4cc348b
Aleksandr Korenev <korenev6052@gmail.com>
2020-01-31 02:19:43 +0300

    16. Хранение состояния

---
+++ src/js/_tests/GameController.test.js
@@ -0,0 +1,28 @@
+import GamePlay from '../GamePlay';
+import GameStateService from '../GameStateService';
+import GameController from '../GameController';
+import loadMock from './mock-consts';
+
+document.body.innerHTML = '<div id="game-container"></div>';
+
+const gamePlay = new GamePlay();
+gamePlay.bindToDOM(document.querySelector('#game-container'));
+
+const stateService = new GameStateService(localStorage);
+
+const gameCtrl = new GameController(gamePlay, stateService);
+gameCtrl.init();
+
+jest.mock('../GameStateService');
+
+test('Load game successfully', () => {
+  gameCtrl.stateService.load.mockReturnValue(loadMock);
+  expect(gameCtrl.onLoadGame()).toBe(true);
+});
+
+test('Load game failed', () => {
+  gameCtrl.stateService.load.mockImplementation(() => {
+    new Error('Invalid state');
+  });
+  expect(gameCtrl.onLoadGame()).toBe(false);
+});
